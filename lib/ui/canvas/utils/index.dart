f929ea770a42ba6595b7be846991e884c98017fe
zhangmj <15669993907@163.com>
2025-11-26 15:17:58 +0800

    文件位置调整

--- lib/ui/canvas/utils/index.dart
+++ lib/ui/canvas/utils/index.dart
@@ -1,4 +1,4 @@
-export 'color_picker_dialog.dart';
-export 'property/color_value_formatter.dart';
-export 'property/border_width_formatter.dart';
+export '../widgets/property/widgets/color_picker_dialog.dart';
+export '../widgets/property/widgets/color_value_formatter.dart';
+export '../widgets/property/widgets/border_width_formatter.dart';
 export 'permission_handler_widget.dart';
